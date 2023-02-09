f46118f5583d19010417c9f865b4e93a314fba47
Gabe Westra <westratechnology@gmail.com>
2023-02-09 04:46:07 -0800

    Added hello world and 90 degree functions

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -25,3 +25,21 @@ async function startProgram() {
  }
 
 }
+
+/**
+ * ninetyDegree function goes forward for a specified amount of time and speed
+ * pausing, turning 90degrees clockwise then moving forward for a specific amount
+ * of speed and time
+*/
+async function ninetyDegree() {
+    await roll((getHeading() + 0), 60, 4); // Heading 0 degrees at 60 speed for 4 seconds
+    await delay(1);
+    await roll((getHeading() + 90), 60, 4); // heading 90 degrees at 60 speed for 4 seconds
+}
+
+/**
+ * startProgram function sends the program to your Sphero Bolt
+*/
+async function startProgram() {
+    await ninetyDegree() //<-- Calling the ninetyDegree fundtion to run it
+}
